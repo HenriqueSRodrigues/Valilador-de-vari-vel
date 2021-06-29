9d9eeaad1bee081e1ef86ae44cf380c4bb280ead
HenriqueSRodrigues <henriquesilvarodrigues.hsr@gmail.com>
2021-06-29 13:29:30 -0300

    validação funcinando

--- js/main.js
+++ js/main.js
@@ -1,47 +1,61 @@
-$(document).ready(function () {
-  // $("#validar").click(function () {
-  function validaBoolean() {
-    let primeiroValor = $("#primeiroValor").val();
-    let segundoValor = $("#segundoValor").val();
-    if (primeiroValor == "true") {
-      primeiroValor = Boolean(true);
-    } else if (primeiroValor == "false") {
-      primeiroValor = Boolean(false);
-    }
-    console.log(primeiroValor);
-    console.log(typeof primeiroValor);
+function validaTipo(valorCampo) {
+  var valorTipado;
+  if (valorCampo == "") {
+    valorTipado = null;
+    console.log(valorTipado);
+  } else if (valorCampo == "true" || valorCampo == "false") {
+    valorTipado = valorCampo == "true";
+  } else if (!isNaN(valorCampo)) {
+    console.log(valorCampo);
+    console.log(typeof valorCampo);
+    valorTipado = Number(valorCampo);
+  } else {
+    valorTipado = valorCampo;
+  }
+  return valorTipado;
+}
 
-    if (segundoValor == "true") {
-      segundoValor = Boolean(true);
-    } else if (segundoValor == "false") {
-      segundoValor = Boolean(false);
-    }
-    console.log(segundoValor);
-    console.log(typeof segundoValor);
+function valida(primeiroValor, segundoValor) {
+  var operador = $("#seletor").val();
+  let resultado;
+
+  if (operador == "==") {
+    resultado = primeiroValor == segundoValor;
+  } else if (operador == "!=") {
+    resultado = primeiroValor != segundoValor;
+  } else if (operador == "===") {
+    resultado = primeiroValor === segundoValor;
+  } else if (operador == "!==") {
+    resultado = primeiroValor !== segundoValor;
   }
-  function validaNumero() {
-    let primeiroValor = $("#primeiroValor").val();
-    let segundoValor = $("#segundoValor").val();
-    if (primeiroValor == !isNaN(true)) {
-      primeiroValor = Number.parseInt(primeiroValor);
-    }
-    if (segundoValor == !isNaN(true)) {
-      segundoValor = Number.parseInt(segundoValor);
-    }
-  }
-  $("#validar").click(function () {
-    validaBoolean();
-    validaNumero();
-  });
-  //   $("#validar").click(function () {
-  //     console.log(typeof primeiroValor);
-  //     if (primeiroValor === segundoValor) {
-  //       console.log("Extritamente igual");
-  //     } else if (primeiroValor == segundoValor) {
-  //       console.log("Igual");
-  //     } else if (primeiroValor != segundoValor) {
-  //       console.log("Diferente");
-  //     } else if (primeiroValor !== segundoValor) {
-  //       console.log("Extritamente diferente");
-  //     }
+  return resultado;
+}
+function tabela(valores, tipos, resultado) {
+  var tabela = $(".tabela").find("tbody");
+  let linha =
+    "<tr>" +
+    "<td>" +
+    valores +
+    "</td>" +
+    "<td>" +
+    tipos +
+    "</td>" +
+    "<td>" +
+    resultado +
+    "</td>" +
+    "</tr>";
+  tabela.prepend(linha);
+}
+
+$("#validar").click(function () {
+  var operador = $("#seletor").val();
+  var primeiroValor = validaTipo($("#primeiroValor").val());
+  var segundoValor = validaTipo($("#segundoValor").val());
+  let valores = primeiroValor.toString() + operador + segundoValor.toString();
+  let resultado = valida(primeiroValor, segundoValor);
+  tabela(
+    valores,
+    typeof primeiroValor + "   " + typeof segundoValor,
+    resultado
+  );
 });
